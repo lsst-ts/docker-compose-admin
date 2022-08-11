1bb8a3384b05e8c972db12c14a7627a3ae799b78
Patrick Ingraham <pingraham@lsst.org>
2022-08-11 11:38:24 -0700

    add support for CBP

--- tucson_teststand/toonie/launch_CalSys_containers.sh
+++ tucson_teststand/toonie/launch_CalSys_containers.sh
@@ -7,7 +7,7 @@ cwd=$(pwd)
 
 # Check to see if a name was given, otherwise assume both
 if [ $# -lt 1 ]; then
-  cscs="electrometer2 electrometer3 electrometer4 electrometer5 laser"  # Will add more when they come online
+  cscs="electrometer2 electrometer3 electrometer4 electrometer5 laser cbp"  # Will add more when they come online
 else
   cscs="$1"
 fi
