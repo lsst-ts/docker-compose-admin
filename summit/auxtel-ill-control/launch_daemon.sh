2ec710d88a9373c1cb8d8565cc66bca571740e09
Patrick Ingraham <pingraham@lsst.org>
2022-12-07 21:50:05 -0300

    Move loonie from the calibration lab to the summit, and rename it auxtel-ill-control. Make scripts executable.

---
+++ summit/auxtel-ill-control/launch_daemon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Launch OSPL daemon on auxtel-ill-control
+
+# Get current working directory
+cwd=$(pwd)
+
+cd /deploy-lsstts/docker-compose-ops/summit
+source setup.env
+cd support
+docker-compose pull ospl-auxtel-ill-control
+docker-compose up -d ospl-auxtel-ill-control
+
+# Return to current working directory
+cd $cwd
