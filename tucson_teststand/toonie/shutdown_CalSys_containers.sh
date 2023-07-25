748f731e3a6fbe4a99a7732d5fec74d8b67f5817
Michael Reuter <mareuter@lsst.org>
2023-07-25 16:32:04 -0700

    TTS: Updates for calibration lab scripts.

--- tucson_teststand/toonie/shutdown_CalSys_containers.sh
+++ tucson_teststand/toonie/shutdown_CalSys_containers.sh
@@ -6,7 +6,7 @@ cwd=$(pwd)
 
 # Check to see if a name was given, otherwise assume both
 if [ $# -lt 1 ]; then
-  cscs="electrometer2 electrometer3 electrometer4  electrometer5 laser"
+  cscs="fiberspectrograph_red electrometer1 electrometer2 electrometer3 electrometer4 electrometer5 laser cbp fiberspectrograph_blue"
 else
   cscs="$1"
 fi
